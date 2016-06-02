1f6500c2425ae2fa8d51ac06e6573aba8ec10ece
sara <sara.fiorendi@cern.ch>
2016-06-02 16:44:06 +0200

    update macro

--- Analyzers/test/readNtuples.C
+++ Analyzers/test/readNtuples.C
@@ -12,7 +12,7 @@
 #include <map>
 
 #include "DataFormats/Math/interface/deltaR.h"
-#include "MuonHLT/Analyzers/src/MuonTree.h"
+#include "MuonHLTNtuples/Analyzers/src/MuonTree.h"
 #include "TLorentzVector.h"
 
 double muonmass = 0.10565837;
@@ -48,51 +48,11 @@ std::string effmeasured    = "L3";
 //                                          *
 // ******************************************
 
-int miss_files[]       = {11, 13, 155, 156, 162, 171, 179, 193, 194, 196, 444, 458, 494, 198, 199, 243, 267, 301, 311, 383, 413, 420, 449, 472};
-int miss_files_L2fix[] = {21, 32, 33, 35, 48, 51, 44, 67, 68, 82, 86, 99, 143, 147, 243, 241, 222, 201, 198};
-int miss_files_273725[] = {134, 120, 191, 12, 14};
-
-std::vector<int> missingfiles;
-int firstfile, lastfile;
-std::string folder, l2string;
-
 void readNtuples(){
 
-//   if (!L2isfixed) {
-//     if (theRunNumber == 273725)  missingfiles.insert(missingfiles.end(), miss_files_273725, miss_files_273725+(sizeof(miss_files_273725)/sizeof(miss_files_273725[0])));
-//     else                         missingfiles.insert(missingfiles.end(), miss_files, miss_files+(sizeof(miss_files)/sizeof(miss_files[0])));
-//   }
-//   else            missingfiles.insert(missingfiles.end(), miss_files_L2fix, miss_files_L2fix+(sizeof(miss_files_L2fix)/sizeof(miss_files_L2fix[0])));
-    
-  if (theRunNumber == 274157 && !L2isfixed){
-    firstfile = 1;
-    lastfile  = 2;
-  } 
-//   else if (theRunNumber == 273555){
-//     firstfile = 136;
-//     lastfile  = 153;
-//   }
-//   else if (theRunNumber == 273725){
-//     firstfile = 1;
-//     lastfile  = 50;
-// //     lastfile  = 214;
-//   }
-//   else if (theRunNumber == 273555 && L2isfixed){
-//     firstfile = 68;
-//     lastfile  = 77;
-//   } 
-//   
-//   if (!L2isfixed)  { 
-//     if (theRunNumber == 273725) {folder = "crab_promptReco_v2_json_273725/160524_145139/"; l2string = "";}
-//     else                        {folder = "crab_promptReco_v2_json_upTo273592/160523_090706/"; l2string = "";}
-//   }
-//   else             { folder = "crab_promptReco_v2_json_upTo273592_L2fix/160523_090802/"; l2string = "_L2fix";}
-  
-  folder = "../../../HLTrigger/Configuration/test/cfgs_2016_5_31_15_25/";
   bool doingL1 = thepassfilter.find("L1fL1") != std::string::npos;
 
-
-  TFile* outfile = TFile::Open(Form("%sefficiency_%d%s.root", effmeasured.c_str(),theRunNumber,l2string.c_str()),"RECREATE");
+  TFile* outfile = TFile::Open(Form("%sefficiency.root", effmeasured.c_str()),"RECREATE");
   std::cout << "output file: " << outfile -> GetName() << std::endl;
   
   TH1F* dimuon_mass             = new TH1F("dimuon_mass"            ,"dimuon_mass"      , 1500,  0,  150 );
@@ -103,9 +63,6 @@ void readNtuples(){
   TEfficiency* muonPt           = new TEfficiency("muonPt"          ,"muonPt"           ,   11,  pt_bins );
   TEfficiency* muonPt_barrel    = new TEfficiency("muonPt_barrel"   ,"muonPt_barrel"    ,   11,  pt_bins );
   TEfficiency* muonPt_endcap    = new TEfficiency("muonPt_endcap"   ,"muonPt_endcap"    ,   11,  pt_bins );
-  TEfficiency* muonPt_eta0      = new TEfficiency("muonPt_eta0"     ,"muonPt_eta0"      ,   11,  pt_bins );
-  TEfficiency* muonPt_eta1      = new TEfficiency("muonPt_eta1"     ,"muonPt_eta1"      ,   11,  pt_bins );
-  TEfficiency* muonPt_eta2      = new TEfficiency("muonPt_eta2"     ,"muonPt_eta2"      ,   11,  pt_bins );
   
   TEfficiency* muonEta          = new TEfficiency("muonEta"         ,"muonEta"          ,   15, eta_bins );
   TEfficiency* muonPhi          = new TEfficiency("muonPhi"         ,"muonPhi"          ,   40, -3.2, 3.2);
@@ -113,135 +70,87 @@ void readNtuples(){
   TEfficiency* nvtx             = new TEfficiency("nvtx"            ,"nvtx"             ,   60,    0,  60);
   TEfficiency* nvtx_barrel      = new TEfficiency("nvtx_barrel"     ,"nvtx_barrel"      ,   60,    0,  60);
   TEfficiency* nvtx_endcap      = new TEfficiency("nvtx_endcap"     ,"nvtx_endcap"      ,   60,    0,  60);
-  TEfficiency* nvtx_eta0        = new TEfficiency("nvtx_eta0"       ,"nvtx_eta0"        ,   60,    0,  60);
-  TEfficiency* nvtx_eta1        = new TEfficiency("nvtx_eta1"       ,"nvtx_eta1"        ,   60,    0,  60);
-  TEfficiency* nvtx_eta2        = new TEfficiency("nvtx_eta2"       ,"nvtx_eta2"        ,   60,    0,  60);
-   
-  TEfficiency* muonIso          = new TEfficiency("muonIso"         ,"muonIso"          ,  100,    0,   1);
-  TEfficiency* muonIso_barrel   = new TEfficiency("muonIso_barrel"  ,"muonIso_barrel"   ,  100,    0,   1);
-  TEfficiency* muonIso_endcap   = new TEfficiency("muonIso_endcap"  ,"muonIso_endcap"   ,  100,    0,   1);
-
-  TH2F* muonPt_2d               = new TH2F("muonPt_2d"              ,"muonPt_2d"        ,   100,   0, 100, 100,    0, 100 );
-  TH2F* muonEta_2d              = new TH2F("muonEta_2d"             ,"muonEta_2d"       ,   500, -2.5, 2.5,  500, -2.5, 2.5 );
-//   TH2F* muonEta_2d              = new TH2F("muonEta_2d"             ,"muonEta_2d"       ,   50, -2.5, 2.5,  50, -2.5, 2.5 );
-  TH2F* muonPhi_2d              = new TH2F("muonPhi_2d"             ,"muonPhi_2d"       ,   320,-3.2, 3.2, 320, -3.2, 3.2 );
    
   double offlineiso04 = 100;
   
-  for (int ifile= firstfile; ifile < lastfile; ifile++){ 
-    if ( std::find(missingfiles.begin(), missingfiles.end(), ifile) != missingfiles.end() ) continue;
-    TFile* inputfile = TFile::Open(Form("%s/muonNtuples_274157_isolation.root",folder.c_str()),"READ");
-    std::cout << "input file: " << inputfile -> GetName() << std::endl;
+  TFile* inputfile = TFile::Open("muonNtuples_274157_isolation.root", "READ");
+  std::cout << "input file: " << inputfile -> GetName() << std::endl;
 
-    TTree *tree = (TTree*) inputfile -> Get("muonNtuples/muonTree");
-    if (!tree) {
-      std::cout << " *** tree not found *** " << std::endl;
-      return;
-    }
+  TTree *tree = (TTree*) inputfile -> Get("muonNtuples/muonTree");
+  if (!tree) {
+    std::cout << " *** tree not found *** " << std::endl;
+    return;
+  }
+    
+  MuonEvent* ev      = new MuonEvent();
+  TBranch*  evBranch = tree->GetBranch("event");
+  evBranch -> SetAddress(&ev);
+
+  int nentries = tree->GetEntriesFast();
+  std::cout << "Number of entries = " << nentries << std::endl;
+
+  bool flagfile = false;
+  for (Int_t eventNo=0; eventNo < nentries; eventNo++)
+  {
+    Int_t IgetEvent   = tree   -> GetEvent(eventNo);
+    
+    if (! (ev-> runNumber == theRunNumber)) break;
+    unsigned int nmuons = ev->muons.size();
+    if (nmuons < 2) continue;
+    
+    unsigned int nhltmuons = ev->hltmuons.size();
+//     if (nhltmuons > 0) std::cout << "Number of hlt muons = " << nhltmuons << std::endl;
+    
+    if (!ev-> hltTag.find(hltname)) continue;
+    nvtx_event      -> Fill( ev -> nVtx   );
+
+    for (int imu = 0; imu < nmuons; imu++){
       
-    MuonEvent* ev      = new MuonEvent();
-    TBranch*  evBranch = tree->GetBranch("event");
-    evBranch -> SetAddress(&ev);
-  
-    int nentries = tree->GetEntriesFast();
-    std::cout << "Number of entries = " << nentries << std::endl;
-  
-    bool flagfile = false;
-    for (Int_t eventNo=0; eventNo < nentries; eventNo++)
-    {
-      Int_t IgetEvent   = tree   -> GetEvent(eventNo);
+      // select the tag muon        
+      if (! selectTagMuon(ev -> muons.at(imu), tagiso))                            continue;
+      if (! matchMuon(ev -> muons.at(imu), ev -> hltTag.objects, isofilterTag))    continue;
+      tagMuonPt -> Fill(ev -> muons.at(imu).pt);
       
-      if (! (ev-> runNumber == theRunNumber)) break;
-//       if (!flagfile){
-//         std::cout << "input file with run 273555: " << inputfile -> GetName() << std::endl;
-//         flagfile == true;
-//       }
-      unsigned int nmuons = ev->muons.size();
-      if (nmuons < 2) continue;
-      
-      unsigned int nhltmuons = ev->hltmuons.size();
-  //     if (nhltmuons > 0) std::cout << "Number of hlt muons = " << nhltmuons << std::endl;
-      
-      if (!ev-> hltTag.find(hltname)) continue;
-      nvtx_event      -> Fill( ev -> nVtx   );
-      
-  
-      for (int imu = 0; imu < nmuons; imu++){
+      for (int jmu = 0; jmu < nmuons; jmu++){
+
+        bool pass   = false;
+ 
+        // select the probe muon
+        if (! selectProbeMuon(ev -> muons.at(jmu), ev -> muons.at(imu), dimuon_mass)) continue;
+
+        offlineiso04 = ev -> muons.at(jmu).chargedDep_dR04 + std::max(0.,
+                       ev -> muons.at(jmu).photonDep_dR04 + ev -> muons.at(jmu).neutralDep_dR04 - 0.5*ev -> muons.at(jmu).puPt_dR04);
+        offlineiso04 = offlineiso04 / ev -> muons.at(jmu).pt;
         
-        // select the tag muon        
-        if (! selectTagMuon(ev -> muons.at(imu), tagiso))                            continue;
-        if (! matchMuon(ev -> muons.at(imu), ev -> hltTag.objects, isofilterTag))    continue;
-        tagMuonPt -> Fill(ev -> muons.at(imu).pt);
+        if (!(offlineiso04 < offlineIsoCut)) continue;
+
+        if (!doingL1 && !(matchMuon(ev -> muons.at(jmu), ev -> hlt.objects, theprobefilter))) continue;
+
+        // match probe muon to the interesting filter and fill numerator histograms
+        if (matchMuon(ev -> muons.at(jmu), ev -> hlt.objects, thepassfilter))       pass = true; 
+
+        muonPt       -> Fill( pass, ev -> muons.at(jmu).pt );
+        muonEta      -> Fill( pass, ev -> muons.at(jmu).eta);
+        muonPhi      -> Fill( pass, ev -> muons.at(jmu).phi);
+        nvtx         -> Fill( pass, ev -> nVtx             );
         
-        for (int jmu = 0; jmu < nmuons; jmu++){
-  
-          bool pass   = false;
-   
-          // select the probe muon
-          if (! selectProbeMuon(ev -> muons.at(jmu), ev -> muons.at(imu), dimuon_mass)) continue;
-  
-          offlineiso04 = ev -> muons.at(jmu).chargedDep_dR04 + std::max(0.,
-                         ev -> muons.at(jmu).photonDep_dR04 + ev -> muons.at(jmu).neutralDep_dR04 - 0.5*ev -> muons.at(jmu).puPt_dR04);
-          offlineiso04 = offlineiso04 / ev -> muons.at(jmu).pt;
-          
-          if (!(offlineiso04 < offlineIsoCut)) continue;
-
-
-          if (!doingL1 && !(matchMuon(ev -> muons.at(jmu), ev -> hlt.objects, theprobefilter))) continue;
-  
-          // match probe muon to the interesting filter and fill numerator histograms
-          if (matchMuon(ev -> muons.at(jmu), ev -> hlt.objects, thepassfilter))       pass = true; 
-  
-          if (pass){
-//             L1MuonCand theL3 = matchL1(ev -> muons.at(jmu), ev -> L1muons);
-            HLTMuonCand theL3 = matchL3(ev -> muons.at(jmu), ev -> L2muons);
-//             HLTMuonCand theL3 = matchL3(ev -> muons.at(jmu), ev -> hltmuons);
-            muonPt_2d  -> Fill( ev -> muons.at(jmu).pt,  theL3.pt );
-            muonEta_2d -> Fill( ev -> muons.at(jmu).eta, theL3.eta);
-            muonPhi_2d -> Fill( ev -> muons.at(jmu).phi, theL3.phi);
-          }
-          
-          muonPt       -> Fill( pass, ev -> muons.at(jmu).pt );
-          muonEta      -> Fill( pass, ev -> muons.at(jmu).eta);
-          muonPhi      -> Fill( pass, ev -> muons.at(jmu).phi);
-          muonIso      -> Fill( pass, offlineiso04           );
-          nvtx         -> Fill( pass, ev -> nVtx             );
-          
-          if (fabs(ev -> muons.at(jmu).eta) < 1.479){
-            muonPt_barrel     -> Fill( pass, ev -> muons.at(jmu).pt );
-            nvtx_barrel       -> Fill( pass, ev -> nVtx );
-            muonIso_barrel    -> Fill( pass, offlineiso04 );
-          }
-          else{
-            muonPt_endcap     -> Fill( pass, ev -> muons.at(jmu).pt );
-            nvtx_endcap       -> Fill( pass, ev -> nVtx             );
-            muonIso_endcap    -> Fill( pass, offlineiso04           );
-          }
-  
-//           if (fabs(ev -> muons.at(jmu).eta) < 0.9){
-//             muonPt_eta0     -> Fill( pass, ev -> muons.at(jmu).pt );
-//             nvtx_eta0       -> Fill( pass, ev -> nVtx );
-//           }
-//           else if (fabs(ev -> muons.at(jmu).eta) > 0.9 && fabs(ev -> muons.at(jmu).eta) < 1.2){
-//             muonPt_eta1     -> Fill( pass, ev -> muons.at(jmu).pt );
-//             nvtx_eta1       -> Fill( pass, ev -> nVtx );
-//           }
-//           else{
-//             muonPt_eta2     -> Fill( pass, ev -> muons.at(jmu).pt );
-//             nvtx_eta2       -> Fill( pass, ev -> nVtx );
-//           }
+        if (fabs(ev -> muons.at(jmu).eta) < 1.479){
+          muonPt_barrel     -> Fill( pass, ev -> muons.at(jmu).pt );
+          nvtx_barrel       -> Fill( pass, ev -> nVtx );
         }
+        else{
+          muonPt_endcap     -> Fill( pass, ev -> muons.at(jmu).pt );
+          nvtx_endcap       -> Fill( pass, ev -> nVtx             );
+        }
+
       }
-    }  
-  } //end loop files
+    }
+  }  
   
   outfile           -> cd();
   muonPt            -> Write();
   muonPt_barrel     -> Write();
   muonPt_endcap     -> Write();
-  muonPt_eta0       -> Write();
-  muonPt_eta1       -> Write();
-  muonPt_eta2       -> Write();
   tagMuonPt         -> Write();
   
   muonEta           -> Write();
@@ -251,21 +160,9 @@ void readNtuples(){
   nvtx              -> Write();
   nvtx_barrel       -> Write();
   nvtx_endcap       -> Write();
-  nvtx_eta0         -> Write();
-  nvtx_eta1         -> Write();
-  nvtx_eta2         -> Write();
   
-  muonIso           -> Write();
-  muonIso_barrel    -> Write();
-  muonIso_endcap    -> Write();
-
   dimuon_mass       -> Write();
   tagiso            -> Write();
-  
-  muonPt_2d         -> Write();
-  muonEta_2d        -> Write();
-  muonPhi_2d        -> Write();
-
   
   outfile           -> Close();  
   
